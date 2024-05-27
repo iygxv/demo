6ba1b9271d80acf18a9e38f8ca2acb5eefa43a53
sy <871772825@qq.com>
2024-05-27 15:53:53 +0800

    feat：lastIndex-in-regex demo

---
+++ lastIndex-in-regex/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import  { ref } from 'vue'
+const value = ref('')
+const errorTips = ref(false)
+const reg = /^1\d{10}$/g
+const handleInput = (e) => {
+  reg.lastIndex = 0
+  if(reg.test(e.target.value)) {
+    errorTips.value = false
+  }else {
+    errorTips.value = true
+  }
+  console.log('lastIndex', reg.lastIndex)
+}
+</script>
+
+<template>
+  <div>
+    手机号：<input v-model="value" placeholder="请输入手机号码" @input="handleInput">
+    <div style="color:red;font-size:12px;margin-top:4px" v-show="errorTips">
+      请输入正确的手机号码
+    </div>
+  </div>
+</template>
+
+<style scoped>
+input {
+  width: 200px;
+  height: 20px;
+}
+</style>
