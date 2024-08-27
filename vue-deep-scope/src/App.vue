a8d01ca9ff8f828f3b70668c0db08db0d7f8a3d2
tianyixuan <871772825@qq.com>
2024-08-27 16:19:25 +0800

    feat: vue-deep-scope + 4种前端切换主题的方式 demo

---
+++ vue-deep-scope/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+   <div class="deep-scope-demo">
+      <h1>deep-scope-demo</h1>
+      <h2>deep-scope-demo</h2>
+      <h3>deep-scope-demo</h3>
+      <h4>deep-scope-demo</h4>
+      <h5>deep-scope-demo</h5>
+      <h6>deep-scope-demo</h6>
+      <!-- <el-input v-model="input" style="width: 240px" placeholder="Please input" /> -->
+   </div>
+</template>
+
+<style scoped lang="scss">
+.deep-scope-demo {
+  // input {
+  //   background-color: skyblue;
+  // }
+  :deep(input) {
+    background-color: skyblue;
+  }
+}
+.deep-scope-demo h1 {
+  color: red;
+}
+.deep-scope-demo h2 {
+  color: green;
+}
+.deep-scope-demo h3 {
+  color: blue;
+}
+.deep-scope-demo h4 {
+  color: rgb(0, 255, 204);
+}
+.deep-scope-demo h5 {
+  color: pink;
+}
+.deep-scope-demo h6 {
+  color: purple;
+}
+
+</style>
